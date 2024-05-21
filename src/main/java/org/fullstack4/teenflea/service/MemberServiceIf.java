6c8afd2921a844f7ea7cf6c86369ecb2cb2d9fa5
jongyun <cjy205205@naver.com>
2024-05-21 15:33:58 +0900

    fetch : member service수정

--- src/main/java/org/fullstack4/teenflea/service/MemberServiceIf.java
+++ src/main/java/org/fullstack4/teenflea/service/MemberServiceIf.java
@@ -1,4 +1,14 @@
 package org.fullstack4.teenflea.service;
 
+import org.fullstack4.teenflea.dto.MemberDTO;
+
 public interface MemberServiceIf {
+
+    int regist(MemberDTO memberDTO);
+
+    MemberDTO view(String user_id);
+
+    void modify(MemberDTO memberDTO);
+
+    void delete(int idx);
 }
--- src/main/java/org/fullstack4/teenflea/service/MemberServiceImpl.java
+++ src/main/java/org/fullstack4/teenflea/service/MemberServiceImpl.java
@@ -2,10 +2,37 @@ package org.fullstack4.teenflea.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.fullstack4.teenflea.domain.MemberEntity;
+import org.fullstack4.teenflea.dto.MemberDTO;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Log4j2
 @Service
 @RequiredArgsConstructor
 public class MemberServiceImpl implements MemberServiceIf{
+    private final ModelMapper modelMapper;
+    /*private final MemberRepository memberRepository;*/
+    @Override
+    public int regist(MemberDTO memberDTO) {
+       /* MemberEntity board = modelMapper.map(boardDTO,BoardEntity.class);
+        int idx = boardRepository.save(board).getIdx();*/
+
+        return 1;
+    }
+
+    @Override
+    public MemberDTO view(String user_id) {
+        return null;
+    }
+
+    @Override
+    public void modify(MemberDTO memberDTO) {
+
+    }
+
+    @Override
+    public void delete(int idx) {
+
+    }
 }
