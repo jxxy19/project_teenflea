ffe722d12299936aec4ac7aa7d61c714af7b7e63
jy <juyoun919@gmail.com>
2024-05-21 13:54:47 +0900

    initial commit

---
+++ src/main/java/org/fullstack4/teenflea/TeenfleaApplication.java
@@ -0,0 +1,13 @@
+package org.fullstack4.teenflea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeenfleaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeenfleaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fullstack4/teenflea/TeenfleaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fullstack4.teenflea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeenfleaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
